cae085c79f4d5f071f3973a0aed3bce1fc935072
javirg1005 <55756129+javirg1005@users.noreply.github.com>
2021-03-25 17:54:27 +0100

    SignUp
    Ventana de sign up y una mainpage temporal

--- Absa/src/app/app.module.ts
+++ Absa/src/app/app.module.ts
@@ -3,11 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { LoginFormComponent } from './components/login-form/login-form.component';
+import { SignupFormComponent } from './components/signup-form/signup-form.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginFormComponent
+    LoginFormComponent,
+    SignupFormComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule
