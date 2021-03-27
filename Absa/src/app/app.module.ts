c1734e751b153f04c3a9dcfd6fbf70b54bbe56ba
javirg1005 <55756129+javirg1005@users.noreply.github.com>
2021-03-27 14:27:33 +0100

    MainPage funcional
    Una pagina para acceder al login y al Sign Up comodamente.
    PD: No es la definitiva

--- Absa/src/app/app.module.ts
+++ Absa/src/app/app.module.ts
@@ -1,11 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+//Rutas para moverse entre ventanas
+
 import { AppComponent } from './app.component';
 import { LoginFormComponent } from './components/login-form/login-form.component';
 import { SignupFormComponent } from './components/signup-form/signup-form.component';
 import { MainPageComponent } from './components/main-page/main-page.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const rutas: Routes = [
+  {
+    path: '', pathMatch:'full', redirectTo:  'MainPage'
+  },
+  {
+    path: 'MainPage', component: MainPageComponent,
+    children: [   
+      {
+        path: 'SignupForm', component: SignupFormComponent
+      },
+      {
+        path: 'LoginForm', component: LoginFormComponent
+      }
+    ]
+  },
+  {
+    path: 'SignupForm', component: SignupFormComponent
+  },
+  {
+    path: 'LoginForm', component: LoginFormComponent
+  }
+]
 
 @NgModule({
   declarations: [
@@ -15,7 +40,11 @@ import { MainPageComponent } from './components/main-page/main-page.component';
     MainPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(rutas, {
+      enableTracing: true, //para un mejor debug
+
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
