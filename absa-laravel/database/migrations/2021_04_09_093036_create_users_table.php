4ac8382f940b2e7c13e54ac11e50b03ea24ee6ca
Caco <55552731+cacolyoko@users.noreply.github.com>
2021-04-09 11:38:16 +0200

    Actualizada la table Users

--- absa-laravel/database/migrations/2021_04_09_093036_create_users_table.php
+++ absa-laravel/database/migrations/2021_04_09_093036_create_users_table.php
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('surname');
             $table->string('email')->unique();
+            $table->rememberToken();
             $table->timestamps();
         });
     }
