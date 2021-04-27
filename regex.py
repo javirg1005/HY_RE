24341d655e9a0f256f410b840e1f06823b8d30ea
alvarobarriada <46595238+alvarobarriada@users.noreply.github.com>
2021-04-27 10:15:35 +0200

    Regex salario corregida
    Listo para unir todo en un diccionario

--- regex.py
+++ regex.py
@@ -1,5 +1,5 @@
-import re
 # Expresiones regex para extraer datos de Infojobs
+import re
 
 # Leemos el fichero con los resultados del GET Offer
 f = open("resultado_infojobs_primero.txt", "r", encoding="utf-8")
@@ -18,9 +18,21 @@ provincia = re.findall(r'province":\s{\s*"id":\s\d+,\s+"value":\s"(\w+\s*)"', da
 url = re.findall(r'(https:\/\/www.infojobs.net\/[\w+\s*]*\/[\w+\s*\-*\.*]*\/[\w+\s*\-*]*)', data)
 salario = re.findall(r'salaryMin":\s{\s*"id":\s\w*,\s*"value":\s"(\d*\.\d*)*', data)
 
-for i in salario:
-    print(i)
+aux=[]
+for i in range(len(salario)):
+    if (len(salario[i]) > 1):
+        aux.append(salario[i])
+        print(aux[i])
+    else:
+        aus = salario[i].replace("", "TBD")
+        aux.append(aus)
+        print(aux[i])
 
+oferta = {'titulo': titulo, 'provincia': provincia, 'url': url, 'salario:'salario }
+
+print(oferta)
+
+print(oferta[i])
 
 # Cerramos el fichero
 f.close()
