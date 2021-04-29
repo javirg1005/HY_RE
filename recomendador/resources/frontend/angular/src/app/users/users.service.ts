04bcd58073f335a788306ae919ade5f749c1bbc3
alvarobarriada <46595238+alvarobarriada@users.noreply.github.com>
2021-04-29 19:08:40 +0200

    Parte Angular copiada
    Del proyecto absa-laravel
    
    Co-Authored-By: javirg1005 <55756129+javirg1005@users.noreply.github.com>
    Co-Authored-By: Caco <55552731+cacolyoko@users.noreply.github.com>

---
+++ recomendador/resources/frontend/angular/src/app/users/users.service.ts
@@ -0,0 +1,32 @@
+// src/app/users/users.service.ts
+
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+/*import { Observable } from "rxjs/Observable";*/
+import { CookieService } from "ngx-cookie-service";
+
+@Injectable({
+    providedIn: "root"
+})
+export class UsersService {
+    httpClient: any; /* A ver si funciona */ 
+    constructor(private http: HttpClient, private cookies: CookieService) {}
+
+    /*login(user: Any): Observable<any> {
+        return this.http.post("RUTA API KAWAII", user);
+    }*/
+
+    login(datos){
+        return this.httpClient.post('http://127.0.0.1:8000/api/login',datos);
+      }
+
+    /*register(user: Any): Observable<any> {
+        return this.http.post("RUTA API KAWAII", user);
+    }
+    setToken(token: String) {
+        this.cookies.set("token", token);
+    }
+    getToken() {
+        return this.cookies.get("token");
+    }*/
+} 
