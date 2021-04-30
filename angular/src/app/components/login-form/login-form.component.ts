81cfbf95b20e04319ba8edb533d7503d013e7eb1
alvarobarriada <46595238+alvarobarriada@users.noreply.github.com>
2021-04-30 17:25:40 +0200

    Revert "RollBack 2 steps"
    This reverts commit 04866dd5a73070e0ea89b276bf1c0423dc0415bc.

--- angular/src/app/components/login-form/login-form.component.ts
+++ angular/src/app/components/login-form/login-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UsersService } from "../../users/users.service";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-login-form',
@@ -14,14 +15,10 @@ export class LoginFormComponent implements OnInit {
   ngOnInit(): void {
   }
   
-  constructor(public userService: UsersService) { }
-
+  constructor(private httpClient:HttpClient, userService: UsersService) { }
   
 
-  login(){
-    const user = {usuario: this.usuario, password: this.password};
-    this.userService.login(user).subscribe( data => {
-      console.log(data);
-    });
+  login(datos){
+    return this.httpClient.post('http://127.0.0.1:8000/api/users', datos);
   }
 }
