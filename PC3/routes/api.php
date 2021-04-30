c122c917f658d13d3fbd1fdd058190f7b0dbd86a
javirg1005 <55756129+javirg1005@users.noreply.github.com>
2021-04-30 16:54:13 +0200

    No PEAR no more
    Co-Authored-By: Alvaro Barriada <46595238+alvarobarriada@users.noreply.github.com>

--- PC3/routes/api.php
+++ PC3/routes/api.php
@@ -18,4 +18,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/auth/users', 'UserController@index');
+Route::post('register', [App\Http\Controllers\API\AuthController::class, 'register'])->name('register');
