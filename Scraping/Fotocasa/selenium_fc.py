0af5bea319614c4e70d70d1f32507ebcd16980f5
alvarobarriada <46595238+alvarobarriada@users.noreply.github.com>
2021-05-06 09:16:03 +0200

    Chromedriver y avances selenium
    Archivo necesario para el screen scraping y avances en el código de selenium, donde ya salta el banner de cookies y realiza una búsqueda

--- Scraping/Fotocasa/selenium_fc.py
+++ Scraping/Fotocasa/selenium_fc.py
@@ -6,25 +6,34 @@ from bs4 import BeautifulSoup
 from selenium.webdriver.common.action_chains import ActionChains
 
 #driver = webdriver.Chrome(executable_path=r'chromedriver')
-driver = webdriver.Chrome('/path/to/chromedriver')
+driver = webdriver.Chrome('chromedriver')
 driver.get("https://www.fotocasa.es/es")
 driver.maximize_window()
+driver.implicitly_wait(5) # Tiempo de espera (inicialmente 10s)
+try:
+    driver.find_element_by_xpath('//*[@class="sui-AtomButton sui-AtomButton--primary sui-AtomButton--solid sui-AtomButton--center "]').click()
+except:
+    time.sleep(2)
 
-time.sleep(2)
+"""#TODO: Revisar ruta del re-Search-selectorContainer
 alquilar = driver.find_element_by_xpath('.//div[@class="re-Search-selectorContainer re-Search-selectorContainer--rent"]')
-alquilar.click()
+alquilar.click()"""
 
 time.sleep(1)
 buscador = driver.find_element_by_xpath('.//div[@class="sui-MoleculeAutosuggest-input-container"]/input')
 buscador.click()
-buscador.send_keys('Málaga')
+
+localidad = 'Oviedo' # Campo a rellenar por el usuario
+
+buscador.send_keys(localidad) #Nombre de la ciudad o pueblo a buscar
 time.sleep(1)
 buscador.send_keys(Keys.ENTER)
 
 html_txt = driver.page_source
 soup = BeautifulSoup(html_txt)
 listaProductos = []
-productos = soup.find_all('div', class_="re-Card-priceComposite")
+productos = soup.find_all('div', class_="re-Card-secondary")
+
 for producto in productos:
     titulo=producto.find('span').getText()
     listaProductos.append(titulo)
