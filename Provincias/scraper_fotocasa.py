597365e7432c58250887a81e526f75067229e01d
alvarobarriada <46595238+alvarobarriada@users.noreply.github.com>
2021-05-29 18:51:32 +0200

    Scraper fotocasa saca varios campos
    Arreglando el tema del precio

--- Provincias/scraper_fotocasa.py
+++ Provincias/scraper_fotocasa.py
@@ -9,51 +9,80 @@ from bs4 import BeautifulSoup
 url_base = "https://www.fotocasa.es/es/comprar/viviendas/"
 localidad = "oviedo" ## Localidad a introducir por el usuario
 url = url_base + localidad +"/todas-las-zonas/l?"
-#print(url)
+print(url)
 
 # Hacemos la petición
 page = requests.get(url)
-#print(page.status_code)
+print(page.status_code)
 
 # Imprimimos el contenido de la página
-#print(page.content)
 soup = BeautifulSoup(page.content, 'html.parser')
 #print(soup)
 
 # Se escribe en un archivo la salida del HTML para poder pasarlo a regex más fácilmente
-file = open("html_general.txt", "w+")
+file = open("html_general.txt", "w+", encoding="utf-8")
 file.write(str(soup))
 
-# Por regex se saca la URL de cada piso
-regex = 'primary"><a href="(\/es\/.*?\/.*?\/.*?\/.*?\/.*?\/.")'
-resultado_regex = re.search(regex, str(soup))
-print(resultado_regex[1])
- 
-## TODO: meter todos los resultados de la regex en un array
-array_regex = []
+articulos = soup.findAll("article")
+urls = []
 
-#https://stackoverflow.com/questions/26438345/how-to-print-regex-match-results-in-python-3
-"""
+# Aplicamos regex
+for articulo in articulos:
+    regex = 'primary"><a href="(\/es\/.*?\/.*?\/.*?\/.*?\/.*?\/.)"'
+    resultado_regex = re.search(regex, str(articulo))
+    if resultado_regex != None:
+        urls.append(resultado_regex.group(1))
 
-## ------------------------------------ Meter en un bucle for ------------------------------------ ##
-
-# Prueba con una URL
+# Conseguir urls de los pisos
 url_fotocasa = "https://www.fotocasa.es"
-url_teatinos = "/es/comprar/vivienda/oviedo/calefaccion-parking-terraza-trastero-ascensor/158992570/d"
-url_piso = url_fotocasa + url_teatinos
-print(url_piso)
+viviendas = []
+
+for i in range (len(urls)):
+    url_piso = url_fotocasa + urls[i]
+    print(url_piso)
+    viviendas.append(url_piso)
 
 # Una vez tenemos la URL de la vivienda en concreto, la scrapeamos
-vivienda = requests.get(url_piso)
-print(vivienda.status_code)
+for i in range(0, len(viviendas)):
+    vivienda = requests.get(viviendas[i])
+    print(vivienda.status_code)
 
-# Imprimimos el contenido de la página
-print(vivienda.content)
-soup_casa = BeautifulSoup(vivienda.content, 'html.parser')
-print(soup_casa)
+    # Imprimimos el contenido de la página
+    #print(vivienda.content)
+    soup_casa = BeautifulSoup(vivienda.content, 'html.parser')
+    #print(soup_casa)
 
-# Se escribe en un archivo la salida del HTML de la vivienda para poder pasarlo a regex más fácilmente
-file = open("html_vivienda.txt", "w+")
-file.write(str(soup_casa))
+    # Se escribe en un archivo la salida del HTML de la vivienda para poder pasarlo a regex más fácilmente
+    file = open("html_vivienda.txt", "w+", encoding="utf-8")
+    file.write(str(soup_casa))
+    
 
-"""
+    ## ------------ Scraper por vivienda --------------- ##
+
+    # Título
+    regexTitulo = '<h1 class="re-DetailHeader-propertyTitle">(.*)<\/h1>'
+    titulo = re.search(regexTitulo, str(soup_casa))
+    print(titulo.group(1))
+
+    # Descripción
+
+
+    # Precio
+    regexPrecio = '<span class="re-DetailHeader-price">(.*)€<'
+    precio = re.search(regexPrecio, str(soup_casa))
+    print(precio.group(1), end="€\n")
+
+    # Número Habitaciones
+    regexHabs = '<span>(.)<\/span> habs'
+    habs = re.search(regexHabs, str(soup_casa))
+    print(habs.group(1),end=" habitaciones\n")
+
+    # Número Baños
+    regexBaños = '<span>(.)<\/span> baño'
+    baños = re.search(regexBaños, str(soup_casa))
+    print(baños.group(1), end=" baño/s\n")
+
+    # Número Metros Cuadrados
+
+
+
