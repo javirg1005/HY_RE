eef0e793e7b8bf17013d15a5639e62f49fcce2b4
alvarobarriada <46595238+alvarobarriada@users.noreply.github.com>
2021-06-09 21:48:36 +0200

    Scraper cambiado ligeramente

--- Provincias/infoempleo_scraper.py
+++ Provincias/infoempleo_scraper.py
@@ -17,14 +17,15 @@ print(page.status_code)
 
 # Imprimimos el contenido de la página
 soup = BeautifulSoup(page.content, 'html.parser')
-print(soup)
+#print(soup)
 
 # Se escribe en un archivo la salida del HTML para poder pasarlo a regex más fácilmente
 file = open("empleoInfo.txt", "w+", encoding="utf-8")
 file.write(str(soup))
 
-empleos = soup.findAll('<li class="featured">') # No funciona porque lo de dentro de li lo considera al mismo nivel?
-
+empleos = soup.find('<ul class="mt15 positions') # No funciona porque lo de dentro de li lo considera al mismo nivel?
+print("----------------------------")
+print(empleos)
 for i in empleos:
     print(empleos[i])
     """
